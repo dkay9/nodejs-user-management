e093f09d2506575dddb5a8ad834e8de64cbe455a
Kalu David <kaludavid411@gmail.com>
2025-01-03 10:15:57 +0100

    new push

--- server/controllers/customerController.js
+++ server/controllers/customerController.js
@@ -1,3 +1,6 @@
+const Customer = require('../models/Customer')
+const mongoose = require('mongoose')
+
 // GET 
 // Homepage 
 
@@ -11,7 +14,7 @@ exports.homepage = async (req, res) => {
 }
 
 // GET 
-// Homepage 
+// New customer form 
 exports.addCustomer = async (req, res) => {
     const locals = {
         title: "NodeJs",
@@ -19,4 +22,26 @@ exports.addCustomer = async (req, res) => {
     }
 
     res.render('customer/add', locals)
+}
+
+// POST
+// Create New customer
+exports.postCustomer = async (req, res) => {
+
+    console.log(req.body)
+    const newCustomer = new Customer({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        tel: req.body.tel,
+        details: req.body.details,
+        email: req.body.email,
+    })
+
+    try {
+        await Customer.create(newCustomer)
+        res.redirect('/')
+    } catch (error) {
+        console.log(error)
+    }
+
 }
