285c0a531722f44a9d7f065ddd201b58a55911f6
Kalu David <kaludavid411@gmail.com>
2025-01-11 17:23:52 +0100

    new push

--- server/routes/customer.js
+++ server/routes/customer.js
@@ -10,5 +10,6 @@ router.get('/add', customerController.addCustomer);
 router.post('/add', customerController.postCustomer);
 
 router.get('/view/:id', customerController.view);
+router.get('/edit/:id', customerController.edit);
 
 module.exports = router
